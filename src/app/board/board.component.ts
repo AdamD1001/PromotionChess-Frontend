c2a2bf8158a427c89e10a1da849eaa997e03a557
Jason <jdawgerj5@gmail.com>
2020-02-12 19:50:48 -0500

    Added shell of event methods for board component

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -18,28 +18,59 @@ export class BoardComponent implements OnInit {
       position: 'ppppkppp/pppppppp/8/8/8/8/PPPPPPPP/PPPPKPPP',
       draggable: true,
       onChange: onChange,
+      onDrop: onDrop,
+      onMoveEnd: onMoveEnd,
+      onMouseoverSquare: onMouseoverSquare,
+      onMouseoutSquare: onMouseoutSquare,
       onDragStart: onDragStart,
       onDragEnd: onDragEnd
     });
 
-    function onChange (source, piece, position, orientation) {
+    // Sends board changes to move-list component
+    function onChange (oldPos, newPos) {
+      console.log("onChange() was called!: ");
+      console.log("oldPos:");
+      console.log(oldPos);
+      console.log("newPos:");
+      console.log(newPos);
+    }
+
+    // Activates whenever player-drag move has been made
+    function onDrop(source, target, piece, newPos, oldPos, orientation) {
 
     }
 
+    // Activates whenever animation has occurred (AI has made move)
+    function onMoveEnd(oldPos, newPos){
+
+    }
+
+    //Activates whenever mouse enters square
+    function onMouseoverSquare(square, piece, boardPos, orientation){
+
+    }
+
+    //Activates whenever mouse leaves square
+    function onMouseoutSquare(square, piece, boardPos, orientation){
+
+    }
+
+    //TODO: onDragStart should not be needed anymore
     function onDragStart (source, piece, position, orientation) {
-      if(orientation === 'white' && piece.search(/^w/) === -1) {
-        return false;
-      }
-      else {
-        console.log(source)
-      }
+      // if(orientation === 'white' && piece.search(/^w/) === -1) {
+      //   return false;
+      // }
+      // else {
+      //   console.log(source)
+      // }
     }
 
+    //TODO: onDragEnd should not be needed anymore
     function onDragEnd (source, piece, position, orientation) {
-      
+
     }
 
-    
+
   }
   showPosition() {
       console.log(this.board.position().a8)
